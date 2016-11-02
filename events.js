4527862d89d3b48041e3d0bf4f89521057d079fb
rivercrthrs <rivercrud@gmail.com>
2016-11-02 16:49:50 -0700

    push function two

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -7,8 +7,10 @@ function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
   one()
-    
+
   // Your turn! Create a new function called `two`, then call it from here.
+  two ()
+  three ()
 }
 
 function one () {
@@ -23,9 +25,17 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
-
+function two () {
+  var two = document.getElementById('two')
+  two.addEventListener('mouseenter', makeGreen)
+  two.addEventListener('mouseleave', makeWhite)
+}
 // CREATE FUNCTION three HERE
-
+function three () {
+  var three = document.getElementById('three')
+  three.addEventListener('mouseenter', makeOrange)
+  three.addEventListener('mouseleave', makeWhite)
+}
 // CREATE FUNCTION four HERE
 
 // Changes the background color of event's target
@@ -35,4 +45,10 @@ function makeBlue (evt) {
 
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
+}
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green'
+}
+function makeOrange (evt) {
+  evt.target.style.backgroundColor = 'orange'
 }
